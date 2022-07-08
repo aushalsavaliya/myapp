67f71e91711aca514fad728e24c875d19e5f5a49
aushalsavaliya <aushalsavaliya994@gmail.com>
2022-07-08 10:10:46 +0530

    8_7_22

--- src/App.js
+++ src/App.js
@@ -2,6 +2,56 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+
+// let arr = [12,18,27,48];
+
+// let ans = Array.isArray(arr)
+
+// console.log(ans);
+// ===========================================================
+
+  // let grid = [12,23,45,67,29,"x"]
+
+  // console.log(grid);
+
+
+
+// ================================================
+    // Arrow finction
+    
+    // const display = (Name,id) =>{
+    //   console.log(Name,id);
+    // }
+    // display("aushal",9)
+
+    // =================================================================
+
+    // block scope
+
+    // const x = 5;  ==> global scope
+    // {
+    //   const x = 10;  ==>  local scope
+
+    //   console.log(x);  ==>  10
+    //  }
+
+    // console.log(x);  5
+// ==================================================
+
+
+  //  const x = 10
+   
+  //  x + x + 5;
+  //  console.log(x);
+
+  // ==================================================
+
+  // let x = 10;
+  
+  // x = x + 5;
+  // console.log(x);
+
   return (
     <div className="App">
       <header className="App-header">
