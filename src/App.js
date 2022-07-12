2d60ca4d3d400f4689beccd65248d2d32cde143a
aushalsavaliya <aushalsavaliya994@gmail.com>
2022-07-12 10:26:48 +0530

    12_7_arraymarging

--- src/App.js
+++ src/App.js
@@ -2,6 +2,58 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  // Array
+
+  //let arr = [1,2,3,4,5,6,7]
+
+  //copy
+
+  //let arr1 = [...arr];
+  //console.log(...arr1);
+
+  //1 2 3 4 5 6 7  line 13
+
+
+  //merging
+
+  //let arr = [1,2,3,4,5,6,7];
+  //let arr2 = [9,10];
+  //let ans = [...arr, ...arr2];
+  //console.log(ans);  //[1, 2, 3, 4, 5, 6, 7, 9, 10]
+
+
+  // destructuring
+  // let arr = [1,2,3,4,5,6];
+  // const [a,b,c,d,e,f] = arr;
+  // console.log(a,f); 
+
+  //1 6
+
+
+  // ======================Object======================
+
+  // copy
+
+  // let obj = {id: 1010 , name: 'aushal'};
+
+  // let obj1 = {...obj};
+  // console.log(obj1);  //{id: 1010, name: 'aushal'}
+
+  //merge obj
+  // let obj2 = {id: 1020 , place: 'surat'};
+  // let ans = {...obj2, ...obj2};
+  // console.log(ans);  //{id: 1020, place: 'surat'}
+
+
+  //destructuring
+
+  // let obj = {id: 1030 , name: 'aushal'};
+  // let {id , name}= obj;
+  // console.log(name , obj .id);
+
+  // aushal 1030
+
   return (
     <div className="App">
       <header className="App-header">
