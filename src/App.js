5c29b8c383af4491a89f640d0e2a247b55f60317
aushalsavaliya <aushalsavaliya994@gmail.com>
2022-07-14 10:46:57 +0530

    14-7-student-data

--- src/App.js
+++ src/App.js
@@ -2,78 +2,58 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  let data=
+ let data=
   [
-    {
-      name: "amit",
-      age: 35,
-      salary: 40000,
-      bonus: 1000,
-      status: true
-    },
-    {
-      name: "ajay",
-      age: 25,
-      salary: 38000,
-      bonus: 2000,
-      status: false
-    },
-    {
-      name: "mayur",
-      age: 23,
-      salary: 50000,
-      bonus: 500,
-      status: true
-    },
-    {
-      name: "jay",
-      age: 29,
-      salary: 35000,
-      bonus: 600,
-      status: true
-    },
-    {
-      name: "raj",
-      age: 33,
-      salary: 22000,
-      bonus: 2000,
-      status: true
-    },
-  ]
+  {
+    grid: 123,
+    name: 'Amit',
+    fees: 50000,
+    attendance: 80
+  },
+  {
+    grid: 456,
+    name: 'Ajay',
+    fees: 70000,
+    attendance: 90
+  },
+  {
+    grid: 856,
+    name: 'Ashish',
+    fees: 30000,
+    attendance: 65
+  },
+  {
+    grid: 762,
+    name: 'Raj',
+    fees: 90000,
+    attendance: 70
+  },
+ ]
 
+  let sdata = data.filter((v,i) => v.attendance > 71);
+  console.log(sdata);
 
-
- 
-  let mdata = data.filter((v,i) => v.status === true);
-  console.log(mdata);
-
-  let totalsalary = mdata.reduce((acc, v, i) => acc + v.salary + v.bonus , 0)
-  console.log(totalsalary);
-
-  let total = data.map((v,i) => v.salary + v.bonus)  
+  let total = sdata.reduce((acc,v,i) => acc + v.fees ,0)
   console.log(total);
-
-
   return (
-   <div>
-    {
-      data.map((v,i) => {
-        return(
-          <div key = {i}>
-            <h1>{v.name}</h1>
-            <h4>{v.salary }</h4>
-            <h4>{v.bonus}</h4>
-            <h4>total</h4>
-            <h4>{v.salary + v.bonus}</h4>
-          </div>
-        )
-      })
-    }
-      <div>
-        <h3>totalsalarys</h3>
-        <h4>{mdata.reduce((acc, v, i) => acc + v.salary + v.bonus , 0)}</h4>     
-      </div>
-   </div>
+    <div>
+      {
+        sdata.map((v,i) => {
+          return(
+            <div key = {i}>
+              <h1>{v.name}</h1>
+              <h3>{v.grid}</h3>
+              <h3>{v.fees}</h3>
+
+            </div>
+          )
+        })
+      }
+      <div>  
+          <h4>total :- {total}</h4>
+        </div>
+    </div>
+    
   );
 }
 
