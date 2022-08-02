b819cd3e38a74a28551b535c32dc8eaf052ea173
aushalsavaliya <aushalsavaliya994@gmail.com>
2022-08-02 14:46:59 +0530

    satate_countryname

--- src/Container/ClassBasedcountry.js
+++ src/Container/ClassBasedcountry.js
@@ -7,19 +7,19 @@ class ClassBasedDemo extends Component {
     constructor(props){
         super(props);
         this.state = {
-            cityname: 'pakistan'
+            countryname: 'pakistan'
         }
     }
     countryname = () => {
         this.setState({
-            cityname: 'Balochistan'
+            countryname: 'Balochistan'
         })
     }
 
     render() {
         return (
             <div>
-                <p>{this.state.cityname}</p>
+                <p>{this.state.countryname}</p>
                 <button onClick={() => this.countryname()}>Countryname</button>
             </div>
         );
